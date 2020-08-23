5831f44e5ef063d8389b215b0d27450b7a03d499
Quiz Dev <contact.quizdeveloper@gmail.com>
2020-08-23 11:03:24 +0700

    Add files via upload

---
+++ MiddlewareExample/MiddlewareExample/Middleware/CustomAuthentication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MiddlewareExample.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CustomAuthentication
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomAuthentication(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // START request http code
+            var userName = httpContext.Request.Query["username"].ToString();
+
+            //if (httpContext.Response.ContentType.Contains("text/html")) // Only html page
+            //{
+                if (userName == null) httpContext.Response.Redirect("/home/login");
+
+                if (httpContext.Request.Path != "/home/login")
+                {
+                    if (userName != "admin" && userName != "administrator")
+                    {
+                        httpContext.Response.Redirect("/home/login");
+                    }
+                }
+           // }
+            // END request http code
+
+            await _next(httpContext);
+
+            // START response http code
+              
+            // END response http code
+
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class CustomAuthenticationExtensions
+    {
+        public static IApplicationBuilder UseCustomAuthentication(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomAuthentication>();
+        }
+    }
+}
---
+++ MiddlewareExample/MiddlewareExample/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiddlewareExample.Middleware;
+
+namespace MiddlewareExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseCustomAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            // Middleware help redirect from http to https.
+            app.UseHttpsRedirection();
+
+            // Middleware help enable static file such as image, css, js..
+            app.UseStaticFiles();
+
+            // Middleware config route for site.
+            app.UseRouting();
+
+            // Middleware enable authentication, by default using Idenity.
+            app.UseAuthorization();
+
+            // Middleware listen and regist router.
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
